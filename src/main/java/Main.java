64366034bb86cb29edc1a593d7bdbca43777cec4
Aman Shaw <amanshaw4511@protonmail.com>
2025-01-04 16:17:19 +0530

    Respond to multiple PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+    private static BufferedReader reader;
+    private static BufferedWriter writer;
+
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
@@ -19,13 +22,22 @@ public class Main {
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
 
-            String message = receiveMessage(clientSocket);
-            while (message != null) {
-                if (message.contains("PING")) {
-                    sendMessage(clientSocket, "+PONG\r\n");
+            setReader(clientSocket);
+            setWriter(clientSocket);
+
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println("Received: " + line);
+                if (line.equalsIgnoreCase("PING")) {
+                    writer.write("+PONG\n\r");
+                    writer.flush();
                 }
-                message = receiveMessage(clientSocket);
+
+                line = reader.readLine();
             }
+
+
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
@@ -39,21 +51,13 @@ public class Main {
         }
     }
 
-    public static String receiveMessage(Socket socket) throws IOException {
+    public static void setReader(Socket socket) throws IOException {
         var inStream = socket.getInputStream();
-        var reader = new BufferedReader(new InputStreamReader(inStream));
-        var message =  reader.readLine();
-
-        System.out.println("received: " + message);
-        return message;
+        reader = new BufferedReader(new InputStreamReader(inStream));
     }
 
-    public static void sendMessage(Socket socket, String message) throws IOException {
-        System.out.println("sending message " + message);
+    public static void setWriter(Socket socket) throws IOException {
         var outStream = socket.getOutputStream();
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
-        writer.write(message);
-        writer.flush();
-        System.out.println("send");
+        writer = new BufferedWriter(new OutputStreamWriter(outStream));
     }
 }
